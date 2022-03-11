cac80a13bd7de81f14fe4c9f33c8778a7fb98c93
Nicolás Táutiva <33504315+NickATC@users.noreply.github.com>
2022-03-11 11:24:05 -0500

    Initial Commit

---
+++ DesitionMaker/DesitionMakerApp.swift
@@ -0,0 +1,17 @@
+//
+//  DesitionMakerApp.swift
+//  DesitionMaker
+//
+//  Created by NICOLAS  TAUTIVA on 11/03/22.
+//
+
+import SwiftUI
+
+@main
+struct DesitionMakerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
